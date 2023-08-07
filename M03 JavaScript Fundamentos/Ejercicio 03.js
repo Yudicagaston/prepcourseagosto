1163140151bf4dcb68e48d899598c0523e6409d4
Yudicagaston <gastonyudicaz@gmail.com>
2023-08-07 18:43:17 -0300

    mi primer commit

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -5,38 +5,57 @@ function sonIguales(x, y) {
    // Retorna true si "x" e "y" son iguales.
    // De lo contrario, retorna false.
    // Tu código:
+   if (x === y) {
+      return true;
+   } else {
+      return false;
+   }
 }
-
+//console.log(sonIguales(5, 5))
 function tienenMismaLongitud(str1, str2) {
    // Retorna true si los dos strings tienen la misma longitud.
    // De lo contrario, retorna false.
    // Tu código:
+   if (str1.length === str2.length) {
+      return true;
+   } else {
+      return false;
+   }
 }
+//console.log(tienenMismaLongitud('pio', 'pororo'));
+
 
 function menosQueNoventa(num) {
    // Retorna true si el argumento "num" es menor que noventa.
    // De lo contrario, retorna false.
    // Tu código:
+   if (num < 90) { return true } else { return false }
 }
-
+//console.log(menosQueNoventa(100));
 function mayorQueCincuenta(num) {
    // Retorna true si el argumento "num" es mayor que cincuenta.
    // De lo contrario, retorna false.
    // Tu código:
+   if (num > 50) { return true } else { return false }
+
 }
+//console.log(mayorQueCincuenta(3));
 
 function esPar(num) {
    // Retorna true si "num" es par.
    // De lo contrario, retorna false.
    // Tu código:
+   if (num % 2 == 0) { return true } else { return false }
 }
+//console.log(esPar(4));
 
 function esImpar(num) {
    // Retorna true si "num" es impar.
    // De lo contrario, retorna false.
    // Tu código:
+   if (num % 2 !== 0) { return true } else { return false }
 }
-
+//console.log(esImpar(7));
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
    tienenMismaLongitud,
